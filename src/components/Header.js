58ac80e654bdbd37a0a6180f8ed497accd449444
Mikołaj Pęcak <mikolajpecak@gmail.com>
2018-04-26 18:20:37 +0200

    Add images and update App.js

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export class Header extends React.Component {
+    render() {
+        return (
+            <div className="jumbotron jumbotron-fluid siteHeader">
+                <a href="http://mpecak.pl"><h1>Mikołaj Pęcak</h1>
+                PROGRAMOWANIE Z PASJI</a>
+            </div>
+        );
+    }
+}
