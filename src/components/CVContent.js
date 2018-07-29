7da59ff5ffa1b18687819aea22caac200d199b1b
Mikolaj Pecak <mikolaj@moodup.team>
2018-07-29 19:23:37 +0200

    Update CVContent

--- src/components/CVContent.js
+++ src/components/CVContent.js
@@ -14,110 +14,104 @@ export const CVContent = () => {
                     Curriculum Vitae
                 </div>
                 <hr/>
-                <div>
                 <div className="section">
-                        <span className="sectionHeader">Doświadczenie zawodowe:</span>
-                        <p>
-                            <b>maj 2018 - obecnie<br/>
-                            Właściciel (CEO)</b><br/>
-                            IT Pulse<br/>
-                            Krótki opis stanowiska:<br/>
-                            Zarządzanie firmą tworzącą rozwiązania wykorzystujące technologię internetową
-                        </p>
-                        <p>
-                            <b>czerwiec 2017 - luty 2018<br/>
-                            iOS Developer</b><br/>
-                            Mood Up Sp. z o.o.<br/>
-                            Krótki opis stanowiska:<br/>
-                            Tworzenie oraz rozwijanie aplikacji mobilnych na urządzenia z systemem iOS
-                        </p>
-                        <p>
-                            <b>listopad 2016<br/>
-                            Przedstawiciel na Międzynarodowych Targach Poznańskich</b><br/>
-                            Automobilklub Leszczyński<br/>
-                            Krótki opis stanowiska:<br/>
-                            Przedstawicielstwo Automobilklubu Leszczyńskiego na MTP 'Retro Motor Show'
-                        </p>
-                        <p>
-                            <b>maj 2013 - październik 2016<br/>
-                            Inżynier wyścigowy</b><br/>
-                            AMO Sportsystem<br/>
-                            Krótki opis stanowiska:<br/>
-                            Praca z młodymi zawodnikami w sporcie kartingowym, mechanik, driver's coach
-                        </p>
-                        <p>
-                            <b>grudzień 2015 - styczeń 2016<br/>
-                            Wprowadzanie asortymentu do sklepu internetowego</b><br/>
-                            Eurokarting S.C.<br/>
-                            Krótki opis stanowiska:<br/>
-                            Wprowadzanie asortymentu firmy do sklepu internetowego oraz pomoc przy inwentaryzacji
-                        </p>
-                        <p>
-                            <b>czerwiec 2013 - lipiec 2013<br/>
-                            Student-praktykant</b><br/>
-                            Interbell Sp. z o.o.<br/>
-                            Krótki opis stanowiska:<br/>
-                            Obsługa klienta w serwisie telefonów komórkowych, pomoc w dziale księgowości, prowadzenie profilu firmy na portalu Facebook
-                        </p>
-                    </div>
-                    <div className="section">
-                        <span className="sectionHeader">Znajomość technologii:</span>
-                        <p>
-                            Programowanie obiektowe<br/>
-                            React JS<br/>
-                            Angular 2 (podstawy)<br/>
-                            PHP Symfony 4<br/>
-                            HTML5<br/>
-                            CSS3<br/>
-                            jQuery<br/>
-                            GIT<br/>
-                            WordPress<br/>
-                            Joomla!<br/>
-                            C++ (podstawy)<br/>
-                            C# (podstawy)<br/>
-                            Pascal<br/>
-                            Microsoft Office<br/>
-                            Social media
-                        </p>
-                    </div>
-                    <div className="section">
-                        <span className="sectionHeader">Znajomość języków obcych:</span>
-                        <p>
-                            Język angielski: poziom B2/C1 (zaawansowany)
-                        </p>
-                    </div>
-                    <div className="section">
-                        <span className="sectionHeader">Wykształcenie:</span>
-                        <p>
-                            <b>październik 2016 - obecnie<br/>
-                            Collegium Da Vinci</b><br/>
-                            Kierunek: informatyka, studia inżynierskie<br/>
-                            Specjalizacja: Aplikacje internetowe i mobilne<br/>
-                            Poziom wykształcenia: inżynier
-                        </p>
-                        <p>
-                            <b>wrzesień 2011 - czerwiec 2016<br/>
-                            Uniwersytet Ekonomiczny w Poznaniu</b><br/>
-                            Kierunek: zarządzanie<br/>
-                            Specjalizacja: zarządzanie przedsiębiorstwami<br/>
-                            Poziom wykształcenia: licencjat
-                        </p>
-                    </div>
-                    <div className="section">
-                        <span className="sectionHeader">Organizacje, aktywności i stowarzyszenia:</span>
-                        <p>
-                            <b>czerwiec 2002 - obecnie<br/>
-                            Automobilklub Leszczyński</b><br/>
-                            Dodatkowe informacje:<br/>
-                            Członek Automobilklubu Leszczyńskiego, członek Kadry Narodowej Polskiego Związku Motorowego w Sporcie Kartingowym w latach 2005 - 2010
-                        </p>
-                    </div>
-                    <div className="section">
-                        <span className="sectionHeader">Zainteresowania:</span>
-                        <p>
-                            Motorsport (głównie wyścigi samochodowe, karting, żużel)
-                        </p>
-                    </div>
+                    <span className="sectionHeader">Znajomość technologii:</span>
+                    <p>
+                        ReactJS<br/>
+                        Node.js (Express)<br/>
+                        Swift, Objective-C<br/>
+                        PHP Symfony 4<br/>
+                        Inne: GIT, jQuery, podstawy SQL i MongoDB, WordPress
+                    </p>
+                </div>
+                <div className="section">
+                    <span className="sectionHeader">Doświadczenie zawodowe:</span>
+                    <p>
+                        <b>maj 2018 - obecnie<br/>
+                        Właściciel (CEO)</b><br/>
+                        IT Pulse<br/>
+                        Krótki opis stanowiska:<br/>
+                        Zarządzanie firmą tworzącą rozwiązania wykorzystujące technologię internetową
+                    </p>
+                    <p>
+                        <b>czerwiec 2017 - luty 2018<br/>
+                        iOS Developer</b><br/>
+                        Mood Up Sp. z o.o.<br/>
+                        Krótki opis stanowiska:<br/>
+                        Tworzenie oraz rozwijanie aplikacji mobilnych na urządzenia z systemem iOS
+                    </p>
+                    <p>
+                        <b>listopad 2016<br/>
+                        Przedstawiciel na Międzynarodowych Targach Poznańskich</b><br/>
+                        Automobilklub Leszczyński<br/>
+                        Krótki opis stanowiska:<br/>
+                        Przedstawicielstwo Automobilklubu Leszczyńskiego na MTP 'Retro Motor Show'
+                    </p>
+                    <p>
+                        <b>maj 2013 - październik 2016<br/>
+                        Inżynier wyścigowy</b><br/>
+                        AMO Sportsystem<br/>
+                        Krótki opis stanowiska:<br/>
+                        Praca z młodymi zawodnikami w sporcie kartingowym, mechanik, driver's coach
+                    </p>
+                    <p>
+                        <b>grudzień 2015 - styczeń 2016<br/>
+                        Wprowadzanie asortymentu do sklepu internetowego</b><br/>
+                        Eurokarting S.C.<br/>
+                        Krótki opis stanowiska:<br/>
+                        Wprowadzanie asortymentu firmy do sklepu internetowego oraz pomoc przy inwentaryzacji
+                    </p>
+                    <p>
+                        <b>czerwiec 2013 - lipiec 2013<br/>
+                        Student-praktykant</b><br/>
+                        Interbell Sp. z o.o.<br/>
+                        Krótki opis stanowiska:<br/>
+                        Obsługa klienta w serwisie telefonów komórkowych, pomoc w dziale księgowości, prowadzenie profilu firmy na portalu Facebook
+                    </p>
+                </div>
+                <div className="section">
+                    <span className="sectionHeader">Znajomość języków obcych:</span>
+                    <p>
+                        Język angielski: poziom B2/C1 (zaawansowany)
+                    </p>
+                </div>
+                <div className="section">
+                    <span className="sectionHeader">Wykształcenie:</span>
+                    <p>
+                        <b>październik 2016 - obecnie<br/>
+                        Collegium Da Vinci</b><br/>
+                        Kierunek: informatyka, studia inżynierskie<br/>
+                        Specjalizacja: Aplikacje internetowe i mobilne<br/>
+                        Poziom wykształcenia: inżynier
+                    </p>
+                    <p>
+                        <b>wrzesień 2011 - czerwiec 2016<br/>
+                        Uniwersytet Ekonomiczny w Poznaniu</b><br/>
+                        Kierunek: zarządzanie<br/>
+                        Specjalizacja: zarządzanie przedsiębiorstwami<br/>
+                        Poziom wykształcenia: licencjat
+                    </p>
+                </div>
+                <div className="section">
+                    <span className="sectionHeader">Organizacje, aktywności i stowarzyszenia:</span>
+                    <p>
+                        <b>styczeń 2017 - czerwiec 2017<br/>
+                        Collegium Da Vinci</b><br/>
+                        Dodatkowe informacje:<br/>
+                        Czynna pomoc przy tworzeniu start-upowego projektu aplikacji internetowej (wykorzystanie technologii Ruby on Rails oraz Angular2)
+                    </p>
+                    <p>
+                        <b>czerwiec 2002 - obecnie<br/>
+                        Automobilklub Leszczyński</b><br/>
+                        Dodatkowe informacje:<br/>
+                        Członek Automobilklubu Leszczyńskiego, członek Kadry Narodowej Polskiego Związku Motorowego w Sporcie Kartingowym w latach 2005 - 2010
+                    </p>
+                </div>
+                <div className="section">
+                    <span className="sectionHeader">Zainteresowania:</span>
+                    <p>
+                        Motorsport (głównie wyścigi samochodowe, karting, żużel)
+                    </p>
                 </div>
             </CSSTransitionGroup>
         </div>
